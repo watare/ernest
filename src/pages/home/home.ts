e1dc7ad54aff36f8ac6dd5b6ce6bf59229b8d94c
aurelien <aurelien.watare@gmail.com>
2017-08-30 19:58:53 +0200

    je valide les modificaitons

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { MediaPlugin } from 'ionic-native';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,57 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
-  }
-
+media: MediaPlugin;
+constructor(public navCtrl: NavController,public alertCtrl: AlertController) {}
+showAlert(message) {
+  let alert = this.alertCtrl.create({
+    title: 'Error',
+    subTitle: message,
+    buttons: ['OK']
+  });
+  alert.present();
 }
+ionViewDidEnter(){
+
+this.media = new MediaPlugin('toto.3gp');	
+
+}		
+
+  
+  public startRecording() {
+  try {
+    this.media.startRecord();
+  }
+  catch (e) {
+    this.showAlert('Could not start recording.');
+  }
+}
+
+public stopRecording() {
+  try {
+    this.media.stopRecord();
+  }
+  catch (e) {
+    this.showAlert('Could not stop recording.');
+  }
+}
+
+public startPlayback() {
+  try {
+    this.media.play();
+  }
+  catch (e) {
+    this.showAlert('Could not play recording.');
+  }
+}
+
+public stopPlayback() {
+  try {
+    this.media.stop();
+  }
+  catch (e) {
+    this.showAlert('Could not stop playing recording.');
+  }
+}
+}
+
