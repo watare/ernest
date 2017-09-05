ad140526d44a8d509d252fbd8a5ec0357b3bfc12
aurelien <aurelien.watare@gmail.com>
2017-09-05 20:28:25 +0200

    ajout d'une fonction permettant de générer un Id unique au fichier audio l'enregistrement du fichier audio dans un dossier specifique ne fonctionne pas par conséquent ceci constitu une sauvegarde avant la création d'un nouveau fichier audio avec une nouvelle méthode

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,11 +9,18 @@ import { File } from '@ionic-native/file';
 export class HomePage {
 
 media: MediaPlugin;
+nameMed : string;
 constructor(private file: File,public navCtrl: NavController,public alertCtrl: AlertController) {}
 
+Id(){
+  // Math.random should be unique because of its seeding algorithm.
+  // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+  // after the decimal.
+  return '_' + Math.random().toString(6).substr(2, 9);
+}
 createDir(){
-  this.file.createDir(this.file.dataDirectory,'violon',true).then(_ => this.showSucces('File created')).catch(err => this.showAlert('File not created'));
-  this.file.checkDir(this.file.dataDirectory,'violon').then(_ => this.showSucces('File exists')).catch(err => this.showAlert('File doesn\'t exist'));
+  this.file.createDir('/data','violon',true).then(_ => this.showSucces('File created')).catch(err => this.showAlert('File not created'));
+  this.file.checkDir('/data','violon').then(_ => this.showSucces('File exists')).catch(err => this.showAlert('File doesn\'t exist'));
 }
 showAlert(message) {
   let alert = this.alertCtrl.create({
@@ -23,7 +30,6 @@ showAlert(message) {
   });
   alert.present();
 }
-
 showSucces(message) {
   let alert = this.alertCtrl.create({
     title: 'Success',
@@ -33,8 +39,10 @@ showSucces(message) {
   alert.present();
 }
 ionViewDidEnter(){
-
-this.media = new MediaPlugin('toto.3gp');	
+  
+  this.nameMed =this.Id() + '.wav';
+  this.showSucces(this.file.externalRootDirectory+this.nameMed); 
+  this.media = new MediaPlugin(this.file.externalRootDirectory+this.nameMed);
 
 }		
 
@@ -49,9 +57,12 @@ this.media = new MediaPlugin('toto.3gp');
 }
 
 public stopRecording() {
-  try {
+  try{
     this.media.stopRecord();
-  }
+    setTimeout(this.file.resolveLocalFilesystemUrl(this.nameMed).then(reponse => this.showSucces(reponse)).catch(err =>this.showAlert('fuck')), 5000);
+    this.file.checkDir('/data',this.nameMed).then(_ => this.showSucces('File exists')).catch(err => this.showAlert('File doesn\'t exist'));
+    //this.file.checkDir(this.file.dataDirectory,'lapin').then(_ => this.showSucces('File exists')).catch(err => this.showAlert('File doesn\'t exist'));
+}
   catch (e) {
     this.showAlert('Could not stop recording.');
   }
